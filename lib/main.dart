e1d01f98ac521366d1086a4058af999a21189b29
Jaime Hoyos Dominguez <host/arcserve@FORTOX.LOCAL>
2024-01-21 20:03:06 -0500

    Presentation: Bloc pattern layers created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:todo/home.dart';
+import 'package:todo/app_home.dart';
 import 'firebase_options.dart';
 
 void main() async {
